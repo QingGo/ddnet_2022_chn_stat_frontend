da0e55b4006a0d4454f69154755d53bda0f55b02
QingGo <zyqingjohn@qq.com>
2022-01-17 21:18:17 +0800

    fix race finish count bug, add more finish data

--- src/pages/cards/Card3.tsx
+++ src/pages/cards/Card3.tsx
@@ -7,13 +7,16 @@ class Card3 extends React.Component<UserInfo> {
         var content = `**${this.props.finish_map_count}** 次过图记录中，
             不重复的地图数有 **${this.props.finish_distinct_map_count}** 张，
             **其中:**
-            新过了 ${this.props.finish_novice_new_map_count} 张 Novice 地图，
-            新过了 ${this.props.finish_moderate_new_map_count} 张 Moderate 地图，
-            新过了 ${this.props.finish_brutal_new_map_count} 张 Brutal 地图，
-            新过了 ${this.props.finish_insane_new_map_count} 张 Insane 地图，
-            新过了 ${this.props.finish_solo_new_map_count} 张 Solo 地图，
-            新过了 ${this.props.finish_ddmax_new_map_count} 张 DDMax 地图，
-            新过了 ${this.props.finish_race_map_count} 张 Race 地图，
+            新过了 **${this.props.finish_novice_map_count}** 次 Novice 地图，共 **${this.props.finish_novice_new_map_count}** 张。
+            新过了 **${this.props.finish_moderate_map_count}** 次 Moderate 地图，共 **${this.props.finish_moderate_new_map_count}** 张。
+            新过了 **${this.props.finish_brutal_map_count}** 次 Brutal 地图，共 **${this.props.finish_brutal_new_map_count}** 张。
+            新过了 **${this.props.finish_insane_map_count}** 次 Insane 地图，共 **${this.props.finish_insane_new_map_count}** 张。
+            新过了 **${this.props.finish_solo_map_count}** 次 Solo 地图，共 **${this.props.finish_solo_new_map_count}** 张。
+            新过了 **${this.props.finish_dummy_map_count}** 次 Dummy 地图，共 **${this.props.finish_dummy_new_map_count}** 张。
+            新过了 **${this.props.finish_ddmax_map_count}** 次 DDMax 地图，共 **${this.props.finish_ddmax_new_map_count}** 张。
+            新过了 **${this.props.finish_oldschool_map_count}** 次 OldSchool 地图，共 **${this.props.finish_oldschool_new_map_count}** 张。
+            新过了 **${this.props.finish_race_map_count}** 次 Race 地图，共 **${this.props.finish_race_new_map_count}** 张。
+            新过了 **${this.props.finish_fun_map_count}** 次 Fun 地图，共 **${this.props.finish_fun_new_map_count}** 张。
             你一定是个大佬吧。`;
         return <div className={"page-card page-card-active card"}>
             <ReactMarkdown>{content}</ReactMarkdown>
